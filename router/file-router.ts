61a469e41d848e33c20eb55233cf9b84e05e5b65
Arthur Datsenko-Boos <adacenkoboos@gmail.com>
2022-11-12 20:00:58 +0700

    files done

--- router/file-router.ts
+++ router/file-router.ts
@@ -1,25 +1,30 @@
-const Router = require('express').Router;
-const router = new Router();
-const fileService = require('../services/file-service');
-const ApiError = require('../exceptions/api-error');
+import { Request, Response, NextFunction } from 'express';
+import { Router } from 'express';
+const router = Router();
+import fileService from '../services/file-service';
+import ApiError from '../exceptions/api-error';
+import { AWSError } from 'aws-sdk/lib/error';
 
 // getting media file
-router.get('/:path/:key', async (req, res, next) => {
-  try {
-    const { key, path } = req.params;
-    // aws getobject
-    const media = await fileService.getMedia(`${path}/${key}`);
-    media
-      .createReadStream()
-      .on('error', (error) => {
-        if (error.code === 'AccessDenied') {
-          next(ApiError.NotFound('Файл не найден'));
-        }
-      })
-      .pipe(res);
-  } catch (error) {
-    next(error);
+router.get(
+  '/:path/:key',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { key, path } = req.params;
+      // aws getobject
+      const media = await fileService.getMedia(`${path}/${key}`);
+      media
+        .createReadStream()
+        .on('error', (error: AWSError) => {
+          if (error.code === 'AccessDenied') {
+            next(ApiError.NotFound('Файл не найден'));
+          }
+        })
+        .pipe(res);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-module.exports = router;
+export default router;
--- services/file-service.ts
+++ services/file-service.ts
@@ -1,27 +1,33 @@
-const aws = require('aws-sdk');
-const nanoid = require('nanoid');
-const s3 = new aws.S3({
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  region: process.env.AWS_BUCKET_REGION,
-});
+import aws from 'aws-sdk';
+import { nanoid } from 'nanoid';
+
+const awsConfig: aws.S3.ClientConfiguration = {
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+  region: process.env.AWS_BUCKET_REGION!,
+};
+const s3 = new aws.S3(awsConfig);
 
 class FileService {
-  async awsUpload(file, path) {
-    const params = {
-      Bucket: process.env.AWS_BUCKET_NAME,
+  // upload a file to aws s3 bucket
+  async awsUpload(file: any, path: string) {
+    const params: aws.S3.PutObjectRequest = {
+      Bucket: process.env.AWS_BUCKET_NAME!,
       Key: path + nanoid(36),
       Body: file.data,
     };
     return s3.upload(params).promise();
   }
-  async getMedia(key) {
+  // get a file from aws s3 bucket and send it to the client
+  async getMedia(
+    key: string
+  ): Promise<aws.Request<aws.S3.GetObjectOutput, aws.AWSError>> {
     const data = await s3.getObject({
-      Bucket: process.env.AWS_BUCKET_NAME,
+      Bucket: process.env.AWS_BUCKET_NAME!,
       Key: key,
     });
     return data;
   }
 }
 
-module.exports = new FileService();
+export default new FileService();
