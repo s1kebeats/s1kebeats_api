1ef17f4109796f8769b77775bdf6870b9aa8e058
Arthur Datsenko-Boos <adacenkoboos@gmail.com>
2022-11-15 23:08:15 +0700

    login tests

---
+++ tests/user/login.test.js
@@ -0,0 +1,81 @@
+import request from 'supertest';
+import assert from 'assert';
+import app from '../../build/app.js';
+
+describe('login endpoint', () => {
+  it('post only', async () => {
+    const res = await request(app).get('/api/login');
+    assert.equal(res.statusCode, 404);
+  });
+  it('no login', async () => {
+    const res = await request(app)
+      .post('/api/register')
+      .send({
+        password: 'randompassword',
+      })
+      .set('Content-Type', 'application/json');
+    assert.equal(res.statusCode, 400);
+  });
+  it('no password', async () => {
+    const res = await request(app)
+      .post('/api/register')
+      .send({
+        login: 'random@email.com',
+      })
+      .set('Content-Type', 'application/json');
+    assert.equal(res.statusCode, 400);
+  });
+  it('login with username', async () => {
+    const res = await request(app)
+      .post('/api/login')
+      .send({
+        login: 's1kebeats',
+        password: 'sbeats2005'
+      })
+      .set('Content-Type', 'application/json');
+    assert.equal(res.statusCode, 200);
+    assert.equal(typeof res.body.accessToken, 'string');
+    assert.equal(typeof res.body.refreshToken, 'string');
+    assert.equal(res.body.user.username, 's1kebeats');
+    assert.equal(res.body.user.email, 'adacenkoboos@gmail.com');
+    assert.equal(res.headers['set-cookie'][0].includes('refreshToken='+ res.body.refreshToken), true)
+    assert.equal(res.headers['set-cookie'][0].includes('HttpOnly'), true)
+  });
+  it('login with email', async () => {
+    const res = await request(app)
+      .post('/api/login')
+      .send({
+        login: 'adacenkoboos@gmail.com',
+        password: 'sbeats2005'
+      })
+      .set('Content-Type', 'application/json');
+    assert.equal(res.statusCode, 200);
+    assert.equal(typeof res.body.accessToken, 'string');
+    assert.equal(typeof res.body.refreshToken, 'string');
+    assert.equal(res.body.user.username, 's1kebeats');
+    assert.equal(res.body.user.email, 'adacenkoboos@gmail.com');
+    // assert.equal(res.headers['set-cookie'][0].split(';')[0].split('=')[1], res.body.refreshToken)
+    assert.equal(res.headers['set-cookie'][0].includes(res.body.refreshToken), true)
+    assert.equal(res.headers['set-cookie'][0].includes('HttpOnly'), true)
+  });
+  it('wrong password', async () => {
+    const res = await request(app)
+      .post('/api/login')
+      .send({
+        login: 's1kebeats',
+        password: 'randompassword',
+      })
+      .set('Content-Type', 'application/json');
+    assert.equal(res.statusCode, 400);
+  });
+  it('wrong login', async () => {
+    const res = await request(app)
+      .post('/api/login')
+      .send({
+        login: 'randonusername',
+        password: 'sbeats2005',
+      })
+      .set('Content-Type', 'application/json');
+    assert.equal(res.statusCode, 400);
+  });
+});
