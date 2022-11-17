591b86b6388a80fdc937a95c808082f483d02a2f
Arthur Datsenko-Boos <adacenkoboos@gmail.com>
2022-11-17 21:46:37 +0700

    beat upload tests not working

--- tests/beat/upload.test.js
+++ tests/beat/upload.test.js
@@ -5,10 +5,21 @@ import app from '../../build/app.js';
 describe('beat upload', () => {
   it('post only', async () => {
     const res = await request(app).get('/beat/upload');
-    assert.equal(res.statusCode, 400);
+    assert.equal(res.statusCode, 404);
   });
-  it('post only', async () => {
-    const res = await request(app).get('/beat/upload');
+  it('no name', async () => {
+    const login = await request(app).get('/api/login').send({
+      login: 's1kebeats',
+      password: 'sbeats2005'
+    })
+    const accessToken = login.body.accessToken;
+    const res = await request(app)
+      .post('/beat/upload')
+      .set('Authorization', accessToken)
+      .field('wavePrice', 1000)
+      .attach('wave', 'tests/files/test.wav')
+      .attach('mp3', 'tests/files/test.mp3');
+    console.log(res.body)
     assert.equal(res.statusCode, 400);
   });
 });
