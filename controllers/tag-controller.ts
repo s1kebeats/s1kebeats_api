99c6ddbe6e73fa37e1b2c7e328b6fe700341fedc
Arthur Datsenko-Boos <adacenkoboos@gmail.com>
2022-11-27 22:42:30 +0700

    tags done

--- controllers/tag-controller.ts
+++ controllers/tag-controller.ts
@@ -1,21 +1,27 @@
 import PrismaClient from '@prisma/client';
 import { Request, Response, NextFunction } from 'express';
-import tagService from '../services/tag-service';
+import tagService from '../services/tag-service.js';
 
 class TagController {
-    async getTags(req: Request, res: Response, next: NextFunction) {
-        try {
-            let tags: PrismaClient.Tag[];
-            if (Object.keys(req.query).length) {
-                tags = await tagService.findTags(req.query, req.body.viewed ? +req.body.viewed : 0);
-            } else {
-                tags = await tagService.getTags(req.body.viewed ? +req.body.viewed : 0);
-            }
-            return res.json(tags)
-        } catch (error) {
-            next(error);
-        }
+  async getTags(req: Request, res: Response, next: NextFunction) {
+    try {
+      let tags: PrismaClient.Tag[] | null;
+      if (req.query.name) {
+        tags = await tagService.findTags(
+          req.query.name as string,
+          req.body.viewed ? +req.body.viewed : 0
+        );
+      } else {
+        tags = await tagService.getTags(req.body.viewed ? +req.body.viewed : 0);
+      }
+      return res.json({
+        tags,
+        viewed: +req.body.viewed + tags!.length,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 }
 
 export default new TagController();
--- services/tag-service.ts
+++ services/tag-service.ts
@@ -1,27 +1,30 @@
-import PrismaClient from "@prisma/client";
+import PrismaClient from '@prisma/client';
 
-const prisma = new PrismaClient.PrismaClient()
+const prisma = new PrismaClient.PrismaClient();
 
 class TagService {
-    async findTags({ name }: { name: string }, viewed: number): Promise<PrismaClient.Tag[] | null> {
-        const tags = await prisma.tag.findMany({
-            where: { 
-                name: {
-                    contains: name
-                }
-            },
-            take: 10,
-            skip: viewed,
-        })
-        return tags;
-    }
-    async getTags(viewed: number): Promise<PrismaClient.Tag[]> {
-        const tags = await prisma.tag.findMany({
-            take: 10,
-            skip: viewed,
-        })
-        return tags;
-    }
+  async findTags(
+    name: string,
+    viewed: number
+  ): Promise<PrismaClient.Tag[] | null> {
+    const tags = await prisma.tag.findMany({
+      where: {
+        name: {
+          contains: name,
+        },
+      },
+      take: 10,
+      skip: viewed,
+    });
+    return tags;
+  }
+  async getTags(viewed: number): Promise<PrismaClient.Tag[]> {
+    const tags = await prisma.tag.findMany({
+      take: 10,
+      skip: viewed,
+    });
+    return tags;
+  }
 }
 
 export default new TagService();
