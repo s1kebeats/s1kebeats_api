6aa45a0c152b4304a8cce6e96758889084d86f0f
datsenkoboos <adacenkoboos@gmail.com>
2023-01-01 19:58:26 +0700

    register + login done

---
+++ tests/integrational/register.test.js
@@ -0,0 +1,20 @@
+import request from "supertest";
+import assert from "assert";
+import app from "../../build/app.js";
+
+it("Register + login", async function () {
+  const register = await request(app)
+    .post("/api/register")
+    .send({
+      email: "adacenkoboos@gmail.com",
+      username: "s1kebeats",
+      password: "Sbeats2005",
+    })
+    .set("Content-Type", "application/json");
+  const login = await request(app).post("/api/login").send({
+    username: "s1kebeats",
+    password: "Sbeats2005",
+  });
+  // email not confirmed
+  assert.equal(login.statusCode, 403);
+}).timeout(5000);
