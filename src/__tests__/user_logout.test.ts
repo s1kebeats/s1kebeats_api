5837818216f2b6241a47ac5675efde0e65f63d50
datsenkoboos <adacenkoboos@gmail.com>
2023-01-13 18:57:01 +0700

    user logout tests done

---
+++ src/__tests__/user_logout.test.ts
@@ -0,0 +1,52 @@
+import request from "supertest";
+import prisma from "../client";
+import bcrypt from "bcrypt";
+import app from "./app.js";
+
+beforeAll(async () => {
+  await prisma.user.createMany({
+    data: [
+      {
+        username: "datsenkoboos",
+        password: await bcrypt.hash("Password1234", 3),
+        email: "datsenkoboos@gmail.com",
+        activationLink: "datsenkoboos-activation-link",
+        isActivated: true,
+      },
+    ],
+  });
+});
+
+afterAll(async () => {
+  await prisma.user.deleteMany();
+  await prisma.$disconnect();
+});
+
+it("GET request should return 404", async () => {
+  const res = await request(app).get("/api/logout");
+  await expect(res.statusCode).toEqual(404);
+});
+it("request without refresh token http-only cookie provided, should return 401", async () => {
+  const res = await request(app).post("/api/logout");
+  await expect(res.statusCode).toEqual(401);
+});
+it("request without refresh token http-only cookie provided, should return 401", async () => {
+  const res = await request(app).post("/api/logout").set("Cookie", ["refreshToken=randomToken"]);
+  await expect(res.statusCode).toEqual(401);
+});
+it("providing valid refresh token, should return 200 and invalidate refreshToken", async () => {
+  const login = await request(app).post("/api/login").send({
+    username: "datsenkoboos",
+    password: "Password1234",
+    refresh: true,
+  });
+  const refreshToken = login.headers["set-cookie"][0].split(" ")[0];
+  const res = await request(app)
+    .post("/api/logout")
+    .set("Cookie", refreshToken);
+  await expect(res.statusCode).toEqual(200);
+  const refreshWithDeletedToken = await request(app)
+    .post("/api/refresh")
+    .set("Cookie", refreshToken);
+  await expect(refreshWithDeletedToken.statusCode).toEqual(401);
+});
